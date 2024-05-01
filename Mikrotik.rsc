5d9da7d46f828b0fab83545f9b22578ad4fa3f92
one-loner <one-loner>
2024-05-01 17:40:11 +0300

    Add more addresses.

--- Mikrotik.rsc
+++ Mikrotik.rsc
@@ -1,7 +1,19 @@
 /ip firewall address-list
+add address=185.224.228.0/24 list=fgup_grchc
 add address=185.224.228.0/22 list=fgup_grchc
+add address=185.224.229.0/24 list=fgup_grchc
+add address=185.224.230.0/24 list=fgup_grchc
+add address=195.209.120.0/24 list=fgup_grchc
 add address=195.209.120.0/22 list=fgup_grchc
+add address=195.209.121.0/24 list=fgup_grchc
+add address=195.209.122.0/24 list=fgup_grchc
+add address=195.209.123.0/24 list=fgup_grchc
+add address=212.192.156.0/24 list=fgup_grchc
+add address=212.192.156.0/23 list=fgup_grchc
 add address=212.192.156.0/22 list=fgup_grchc
+add address=212.192.157.0/24 list=fgup_grchc
+add address=212.192.158.0/24 list=fgup_grchc
+add address=212.192.159.0/24 list=fgup_grchc
 
 /ip firewall filter
 add action=reject chain=forward dst-address-list=fgup_grchc protocol=tcp reject-with=tcp-reset
--- iptables_PREROUTING.sh
+++ iptables_PREROUTING.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+/usr/sbin/iptables -A PREROUTING -s 185.224.228.0/24 -j DROP
 /usr/sbin/iptables -A PREROUTING -s 185.224.228.0/22 -j DROP
+/usr/sbin/iptables -A PREROUTING -s 185.224.229.0/24 -j DROP
+/usr/sbin/iptables -A PREROUTING -s 185.224.230.0/24 -j DROP
+/usr/sbin/iptables -A PREROUTING -s 195.209.120.0/24 -j DROP
 /usr/sbin/iptables -A PREROUTING -s 195.209.120.0/22 -j DROP
+/usr/sbin/iptables -A PREROUTING -s 195.209.121.0/24 -j DROP
+/usr/sbin/iptables -A PREROUTING -s 195.209.122.0/24 -j DROP
+/usr/sbin/iptables -A PREROUTING -s 195.209.123.0/24 -j DROP
+/usr/sbin/iptables -A PREROUTING -s 212.192.156.0/24 -j DROP
+/usr/sbin/iptables -A PREROUTING -s 212.192.156.0/23 -j DROP
 /usr/sbin/iptables -A PREROUTING -s 212.192.156.0/22 -j DROP
+/usr/sbin/iptables -A PREROUTING -s 212.192.157.0/24 -j DROP
+/usr/sbin/iptables -A PREROUTING -s 212.192.158.0/24 -j DROP
+/usr/sbin/iptables -A PREROUTING -s 212.192.159.0/24 -j DROP
